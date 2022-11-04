749a94c1d9e34dbcacfab9ade11201b434e3500b
Ankit Jain <ankii.jain@gmail.com>
2022-11-04 17:15:13 +0530

    fix issue

--- variables.tf
+++ variables.tf
@@ -210,7 +210,7 @@ variable "iam_database_authentication_enabled" {
 variable "database_name" {
     description = "Name for an automatically created database on cluster creation."
     type        = string
-    value       = null
+    default     = null
 }
 
 variable "enable_http_endpoint" {
